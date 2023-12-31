17d4baf2b856298c773b08b88cb3cae53df5431c
Marcus <marcusvinicius17031998@gmail.com>
2023-12-31 11:04:14 -0300

    Correção de bugs

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,22 +1,53 @@
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 import { PensamentoService } from './../pensamento.service';
-import { Component, OnInit } from '@angular/core';
-import { Pensamento } from '../pensamento';
+import { Pensamento } from './../pensamento';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
-export class ListarPensamentoComponent implements OnInit {
-
+export class ListarPensamentoComponent implements OnInit, OnDestroy {
   listaPensamentos: Pensamento[] = [];
+  private destroy$ = new Subject<void>();
 
-  constructor(private service: PensamentoService) { }
+  constructor(
+    private service: PensamentoService,
+    private location: Location,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.carregarListaPensamentos();
+    this.setupAutoReload();
+  }
+
+  private carregarListaPensamentos() {
     this.service.listar().subscribe((listaPensamentos) => {
-      this.listaPensamentos = listaPensamentos
-    })
+      this.listaPensamentos = listaPensamentos;
+    });
+  }
+
+  private setupAutoReload() {
+    interval(1000)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.carregarListaPensamentos();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  criar(pensamento: Pensamento) {
+    this.service.criar(pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'], { replaceUrl: true });
+    });
   }
 }
-
